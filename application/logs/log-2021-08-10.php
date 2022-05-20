7dbd7ed0fe03d4f47a62c9b6a82da31d0740b766
Maulana <73470175+myf0506@users.noreply.github.com>
2022-05-20 13:30:54 +0700

    Add files via upload

---
+++ application/logs/log-2021-08-10.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2021-08-10 09:54:24 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\hotel_apps\hotelmanagement\system\core\Common.php 257
+ERROR - 2021-08-10 09:54:24 --> Severity: Warning  --> mysqli_connect(): (HY000/1044): Access denied for user ''@'localhost' to database 'manajemenhotel_db' C:\xampp\htdocs\hotel_apps\hotelmanagement\system\database\drivers\mysqli\mysqli_driver.php 76
+ERROR - 2021-08-10 09:54:24 --> Unable to connect to the database
+ERROR - 2021-08-10 09:54:24 --> The session cookie data did not match what was expected. This could be a possible hacking attempt.
+ERROR - 2021-08-10 09:58:49 --> Severity: Notice  --> Only variable references should be returned by reference C:\xampp\htdocs\hotel_apps\hotelmanagement\system\core\Common.php 257
+ERROR - 2021-08-10 09:58:49 --> Severity: Warning  --> mysqli_connect(): (HY000/1044): Access denied for user ''@'localhost' to database 'manajemenhotel_db' C:\xampp\htdocs\hotel_apps\hotelmanagement\system\database\drivers\mysqli\mysqli_driver.php 76
+ERROR - 2021-08-10 09:58:49 --> Unable to connect to the database
